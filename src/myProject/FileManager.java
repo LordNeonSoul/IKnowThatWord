90ec50df7dde69417be7a80baf7618208749f196
2040104 Juan Pablo Pantoja <juan.pablo.pantoja@correounivalle.edu.co>
2022-02-17 10:14:11 -0500

    FileManager and ModelIKTW added

---
+++ src/myProject/FileManager.java
@@ -0,0 +1,35 @@
+package myProject;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileManager {
+    private FileReader fileReader;
+    private BufferedReader input;
+    private FileWriter fileWriter;
+    private BufferedWriter output;
+
+    public ArrayList<String> usersReader(){
+        ArrayList<String> users = new ArrayList<>();
+        try {
+            fileReader = new FileReader("src/files/Users.txt");
+            input = new BufferedReader(fileReader);
+            String line = input.readLine();
+            while(line!=null){
+                users.add(line);
+                line = input.readLine();
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return users;
+    }
+}
